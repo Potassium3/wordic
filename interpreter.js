2be36093a085010ff9b9aceca1292f5aa519f7b2
Potassium3 <miles.plowman@gmail.com>
2026-09-12 07:45:43 +0000

    save

--- interpreter.js
+++ interpreter.js
@@ -11,15 +11,26 @@ const commands = {
     "new": {
         inputs: 2,
         run: function(args, variables) {
-            variables[args[0]] = parseWord(args[1], variables);
-            return variables, "";
+            let newVariables = variables;
+            newVariables[args[0]] = parseWord(args[1], variables);
+            return [variables, ""];
         }
     },
     "set": {
         inputs: 2,
         run: function(args, variables) {
-            variables[args[0]] = parseWord(args[1], variables);
-            return variables, "";
+            let newVariables = variables;
+            newVariables[args[0]] = parseWord(args[1], variables);
+            return [variables, ""];
+        }
+    },
+    "out": {
+        inputs: 1,
+        run: function(args, variables) {
+            let newVariables = variables;
+            let output = parseWord(args[0], variables);
+
+            return [newVariables, output];
         }
     },
 }
@@ -38,33 +49,62 @@ function parseWord(word, variables) {
     } else if (String(Number(word)) == word) {
         return Number(word); // Parse number
     } else {
-        return word; // Parse string (either operator or literal string)
+        let arr = [];
+        for (let char of word) {
+            arr.push(char);
+        }
+        return arr; // Parse string into array of chars
     }
 }
 
-// Make a parseWordsIntoStructuredList function to structure the operators
+// Make a parseWordsIntoStructuredList function to structure the operators (no, using recursion)
 // Pass this into evaluate at each level
+
+function evaluate(args) {
+    return 0;
+}
 
 function runLine(line, variables) {
     let newVariables = structuredClone(variables);
     let principalCommand = line[0];
     let output = "";
-    console.log(line);
     if (principalCommand in commands) {
-        console.log("principal recognised"+principalCommand);
-        let args = Array(line.length-1);
+        let command = commands[principalCommand];
+        
+        // Format arguments to principal command (first of line)
+        let args = [];
+        let expectedArgs = command.inputs; // Keep track of how many args needed
+        console.log("Initial expected: "+expectedArgs);
+        let initialArgs = expectedArgs;
         let i = 0;
         for (let word of line) {
             if (i != 0) {
-                args[i-1] = word; // Formats words into list
+                if (word in operators) {
+                    expectedArgs += operators[word].inputs - 1; // Additional args expected - operator
+                } else {
+                    if (expectedArgs < initialArgs) {
+                        initialArgs = expectedArgs;
+                        
+                        let arg = [];
+                        for (let j=0; j<i; j++) {
+                            arg.push(args[j]);
+                        }
+                        args.push(evaluate(arg)); // Evaluate all words before
+                    }
+                    expectedArgs--;
+                }
+                console.log("Argument: "+word+", Expected "+expectedArgs+" more.");
             }
             i++;
         }
+        // If expected args != 0 error invalid argument format
         console.log("Args: "+args);
-        let result = commands[principalCommand].run(args, variables); // Runs the line
-        variables = result[0];
-        output += result[1] == "" ? "" : result[1];
-    }
+
+        // Run command
+        let result = command.run(args, variables); // Runs the line
+        newVariables = result[0];
+        output = result[1];
+    } // else error command unrecognised
     return { variables: newVariables, output };
 }
 
@@ -86,13 +126,13 @@ function run(wordic) {
         if (char == "\n" || i==len-1) {
             // Run the line
             let result = runLine(line, variables);
-            //output += result.output;
+            output += result.output;
             variables = result.variables;
-            
             line = [];
         }
         i++;
     }
-    output = "Variables: "+JSON.stringify(variables);
-    return output;
+    let returning = "Variables: "+JSON.stringify(variables);
+    returning += "\nOutput: "+output;
+    return returning;
 }
